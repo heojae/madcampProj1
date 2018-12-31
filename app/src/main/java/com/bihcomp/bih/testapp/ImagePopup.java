aedd3e17c175e5c1d8944d03701e020283ba6371
Hyeonjoon <cs2420@kaist.ac.kr>
2018-12-31 15:56:19 +0900

    Tab2 completed.

---
+++ app/src/main/java/com/bihcomp/bih/testapp/ImagePopup.java
@@ -0,0 +1,97 @@
+package com.bihcomp.bih.testapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.bihcomp.bih.testapp.Fragment2;
+import com.bihcomp.bih.testapp.R;
+
+public class ImagePopup extends FragmentActivity implements OnClickListener {
+    private Context mContext = null;
+    //private final int imgWidth = 960;
+    //private final int imgHeight = 540;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.image_popup);
+        mContext = this;
+
+        // 전송메시지
+        Intent i = ((FragmentActivity) mContext).getIntent();
+        Bundle extras = i.getExtras();
+        String imgPath = extras.getString("filename");
+
+        // 완성된 이미지 보여주기
+        BitmapFactory.Options bfo = new BitmapFactory.Options();
+        bfo.inSampleSize = 2;
+        ImageView iv = (ImageView) findViewById(R.id.imageView);
+        Bitmap bm = BitmapFactory.decodeFile(imgPath, bfo);
+
+        float widthOrigin = bm.getWidth();
+        float heightOrigin = bm.getHeight();
+        int widthFixed;
+        int heightFixed;
+        float scale;
+
+        if (widthOrigin > heightOrigin) {
+            widthFixed = 960;
+            scale = widthFixed / widthOrigin;
+            heightFixed = (int) (heightOrigin * scale);
+        } else {
+            heightFixed = 960;
+            scale = heightFixed / heightOrigin;
+            widthFixed = (int) (widthOrigin * scale);
+        }
+
+        Bitmap resized = Bitmap.createScaledBitmap(bm, widthFixed, heightFixed, true);
+        iv.setImageBitmap(resized);
+    }
+
+    /*
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.image_popup, container, false);
+        mContext = getActivity();
+
+        // 전송메시지
+        Intent i = ((FragmentActivity) mContext).getIntent();
+        Bundle extras = i.getExtras();
+        String imgPath = extras.getString("filename");
+
+        // 완성된 이미지 보여주기
+        BitmapFactory.Options bfo = new BitmapFactory.Options();
+        bfo.inSampleSize = 2;
+        ImageView iv = (ImageView) getView().findViewById(R.id.imageView);
+        Bitmap bm = BitmapFactory.decodeFile(imgPath, bfo);
+        Bitmap resized = Bitmap.createScaledBitmap(bm, imgWidth, imgHeight, true);
+        iv.setImageBitmap(resized);
+
+        // 리스트로 가기 버튼
+        Button btn = (Button) getView().findViewById(R.id.btn_back);
+        btn.setOnClickListener(this);
+
+        return view;
+    }*/
+
+    /* (non-Javadoc)
+     * @see android.view.View.OnClickListener#onClick(android.view.View)
+     */
+    public void onClick(View v) {
+
+    }
+}
--- app/src/main/java/com/bihcomp/bih/testapp/MainActivity.java
+++ app/src/main/java/com/bihcomp/bih/testapp/MainActivity.java
@@ -1,14 +1,18 @@
 package com.bihcomp.bih.testapp;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -38,6 +42,13 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    /***************************************************
+     * Target API 22 이상으로, 수동 권한 요청
+     ***************************************************/
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXT_STORAGE = 1;
+
+
+
     private ViewPager mViewPager;
     android.support.v7.app.ActionBar bar;
     private FragmentManager fm;
@@ -63,6 +74,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
         */
+
+
+        /***************************************************
+         * Target API 22 이상으로, 수동 권한 요청
+         ***************************************************/
+
+        requestReadExternalStoragePermission();
+
+
+
+
+
+
+
+
+
+
+
 
         fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
         fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
@@ -386,5 +415,67 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         return ret;
     }
+
+
+
+
+    /***************************************************
+     * Target API 22 이상으로, 수동 권한 요청
+     ***************************************************/
+
+    private void requestReadExternalStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_EXT_STORAGE);
+                // MY_PERMISSIONS_REQUEST_READ_EXT_STORAGE is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXT_STORAGE : {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+
+
+
+
+
+
+
 
 }
